e756a1eb598f28353b4da0029c2050c373494053
Max <m.li.92@icloud.com>
2021-04-01 21:40:36 +0600

    Apple start

--- testSnake/testSnake/Apple.swift
+++ testSnake/testSnake/Apple.swift
@@ -7,3 +7,19 @@
 
 import UIKit
 import SpriteKit
+
+class Apple: SKShapeNode {
+    
+    convenience init(position: CGPoint) {
+        self.init()
+        
+        path = UIBezierPath(ovalIn: CGRect(x: 0, y: 0, width: 10, height: 10)).cgPath
+        fillColor = UIColor.red
+        strokeColor = UIColor.red
+        lineWidth = 5
+        
+        self.position = position
+        
+    }
+    
+}
